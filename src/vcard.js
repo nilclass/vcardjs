76f4776872df571129ceb1be0c077e38b25461c4
nil <nilclass@riseup.net>
2012-06-03 15:12:59 +0000

    validation, UUID generation, rev generation

--- src/vcard.js
+++ src/vcard.js
@@ -5,21 +5,102 @@ var VCard;
 (function() {
 
     VCard = function() {
+	this.changed = false;
     };
 
     VCard.prototype = {
 
-        // don't set attributes directly. always use this method.
-        // it may call callbacks in the future.
+	// Check validity of this VCard instance. Properties that can be generated,
+	// will be generated. If any error is found, false is returned and vcard.errors
+	// set to an Array of [attribute, errorType] arrays.
+	// Otherwise true is returned.
+	//
+	// In case of multivalued properties, the "attribute" part of the error is
+	// the attribute name, plus it's index (starting at 0). Example: email0, tel7, ...
+	//
+	// It is recommended to call this method even if this VCard object was imported,
+	// as some software (e.g. Gmail) doesn't generate UIDs.
+	validate: function() {
+	    var errors = [];
+
+	    function addError(attribute, type) {
+		errors.push([attribute, type]);
+	    }
+
+	    if(! this.fn) { // FN is a required attribute
+		addError("fn", "required");
+	    }
+
+	    // make sure multivalued properties are *always* in array form
+	    for(var key in VCard.multivaluedKeys) {
+		if(this[key] && ! (this[key] instanceof Array)) {
+		    addError(key, "not-an-array");
+		}
+	    }
+
+	    // make sure compound fields have their type & value set
+	    // (to prevent mistakes such as vcard.addAttribute('email', 'foo@bar.baz')
+	    function validateCompoundWithType(attribute, values) {
+		for(var i in values) {
+		    var value = values[i];
+		    if(typeof(value) !== 'object') {
+			errors.push([attribute + i, "not-an-object"]);
+		    } else if(! value.type) {
+			errors.push([attribute + i, "missing-type"]);
+		    } else if(! value.value) { // empty values are not allowed.
+			errors.push([attribute + i, "missing-value"]);
+		    }
+		}
+	    }
+
+	    if(this.email) {
+		validateCompoundWithType('email', this.email);
+	    }
+
+	    if(this.tel) {
+		validateCompoundWithType('email', this.tel);
+	    }
+
+	    if(! this.uid) {
+		addAttribute('uid', this.generateUID());
+	    }
+
+	    if(! this.rev) {
+		addAttribute('rev', this.generateRev());
+	    }
+
+	    this.errors = errors;
+
+	    return ! (errors.length > 0);
+	},
+
+	// generate a UID. This generates a UUID with uuid: URN namespace, as suggested
+	// by RFC 6350, 6.7.6
+	generateUID: function() {
+	    return 'uuid:' + Math.uuid();
+	},
+
+	// generate revision timestamp (a full ISO 8601 date/time string in basic format)
+	generateRev: function() {
+	    return (new Date()).toISOString().replace(/[\.\:\-]/g, '');
+	},
+
+	// Set the given attribute to the given value.
+	// This sets vcard.changed to true, so you can check later whether anything
+	// was updated by your code.
         setAttribute: function(key, value) {
             this[key] = value;
+	    this.changed = true;
         },
 
+	// Set the given attribute to the given value.
+	// If the given attribute's key has cardinality > 1, instead of overwriting
+	// the current value, an additional value is appended.
         addAttribute: function(key, value) {
             console.log('add attribute', key, value);
             if(VCard.multivaluedKeys[key]) {
                 if(this[key]) {
-                    this[key].push(value);
+                    this.setAttribute(key, this[key].push(value));
                 } else {
                     this.setAttribute(key, [value]);
                 }
@@ -28,6 +109,14 @@ var VCard;
             }
         },
 
+	// convenience method to get a JSON serialized jCard.
+	toJSON: function() {
+	    return JSON.stringify(this.toJCard());
+	},
+
+	// Copies all properties (i.e. all specified in VCard.allKeys) to a new object
+	// and returns it.
+	// Useful to serialize to JSON afterwards.
         toJCard: function() {
             var jcard = {};
             for(var k in VCard.allKeys) {
@@ -89,7 +178,7 @@ var VCard;
                       "sweetheart", "me", "agent", "emergency"],
         // FIXME: these aren't actually defined anywhere. just very commmon.
         //        maybe there should be more?
-        emailType: ["work", "home"],
+        emailType: ["work", "home", "internet"],
         langType: ["work", "home"],
         
     };
